0d3f147432cdeb57ed2532a48443140a2e89d637
mardommah <reskyikky@gmail.com>
2023-06-01 21:23:33 +0800

    add php files

--- array.php
+++ array.php
@@ -11,5 +11,26 @@
 	foreach ($angka as $number) {
 		echo "$number <br>";
 	};
+?>  
 
-?>
+<html>
+  <head>
+    <title>Testing Mahasiswa</title>
+  </head>
+  <body>
+  <?php
+  $test = [
+    ["Test", 123, "asasas"],
+    ["testing", "456", True]
+  ];
+  ?>
+  <h2>Data Mhs</h2>
+  <?php foreach($test as $mhs) : ?>
+  <ul>
+    <li><?php echo $mhs[0];?></li>
+    <li><?php echo $mhs[1];?></li>
+    <li><?php echo $mhs[2];?></li>
+  </ul>
+  <?php endforeach; ?>
+  </body>
+</html>
